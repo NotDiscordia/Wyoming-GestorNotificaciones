c33d2a2fc1e810bdca79f7c712140990c610e1d3
Maximo Julian Roman Rodriguez <134997528+NotDiscordia@users.noreply.github.com>
2024-11-04 02:43:22 -0800

    Archivo fuente del proyecto a realizar
    Archivo del proyecto para IntelliJ

---
+++ Wyoming/WyomingGestor/src/main/java/wyoming/wyominggestor/WyomingGestorApplication.java
@@ -0,0 +1,13 @@
+package wyoming.wyominggestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyomingGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WyomingGestorApplication.class, args);
+	}
+
+}
